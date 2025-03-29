ee7c275365e641953b9bd2195cdf8eb1d4e87f0d
BenBrooke450 <bensir@mail.com>
2025-03-30 00:42:30 +0100

    Basic

---
+++ W3schools - Pandas/Advanced.py
@@ -0,0 +1,24 @@
+
+
+import numpy as np
+import pandas as pd
+
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank'],
+    'Age': [24, 27, 22, 32, 29, 24],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'New York'],
+    'Salary': [70000, 80000, 65000, 120000, 95000, 68000],
+    'Experience': [2, 5, 1, 10, 7, 2]
+}
+
+
+
+# Convert dictionary to DataFrame
+df = pd.DataFrame(data)
+
+df['Salary Rank'] = (df['Salary'].rank())
+
+df['Salary Rank_dense'] = (df['Salary'].rank(method = "dense"))
+
+print(df)
--- W3schools - Pandas/Intermediate.py
+++ W3schools - Pandas/Intermediate.py
@@ -30,3 +30,36 @@ dtype: object
 
 
 df['Age'] = df['Age'].astype(float)
+
+
+
+
+
+
+df['charges_x2'] = df['charges'].apply(lambda x: x*2)
+
+
+
+df['charges'] = df['charges'].apply(lambda x: x*2)
+
+
+
+df[['age', 'charges']] = df[['age', 'charges']].applymap(lambda x: x + 1)
+
+
+
+sampled_df = df.sample(n=3)
+
+
+
+filtered_df = df[df['age'].isin([22, 23])]
+
+
+
+
+df['Salary Rank'] = (df['Salary'].rank())
+df = df.sort_values('Salary Rank')
+
+
+
+
