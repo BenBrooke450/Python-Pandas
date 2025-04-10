ab267af903cd40d9d3024b05941b5f2621c8aff8
BenBrooke450 <bensir@mail.com>
2025-04-10 23:54:34 +0200

    Basic

--- Pandas Practice/Playground/4.DAY(pop()).py
+++ Pandas Practice/Playground/4.DAY(pop()).py
@@ -1,3 +1,23 @@
 
 
 
+
+
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris, fetch_california_housing
+
+housing = fetch_california_housing()
+
+df = pd.DataFrame(housing.data, columns=housing.feature_names)
+
+print(df)
+
+df.pop("Longitude") # REMOVES INPLACE
+# ALSO CAN ONLY REMOVE ONE AT A TIME
+
+df.drop(columns = ["Latitude","AveOccup"],inplace=True) #NEEDS TO PUT INPLACE
+#CAN HANDLE MULTIPLE
+
+
+
